5cdff655088aec990467e539cfd3bce9a919bac0
ALEKSANDR.I <107239203+AlexRMB1455AL@users.noreply.github.com>
2025-07-04 16:57:57 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+//echo 111111111155;
+
+$num = 10;
+$float = 10.10;
+
+$str = "Hello World! $num ";
+$str = 'Hello World! $num';
+$num  *=  2;
+
+echo "The number is $num.";
+echo "The number is $str.";
